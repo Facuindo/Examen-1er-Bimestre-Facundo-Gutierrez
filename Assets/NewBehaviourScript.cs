fa0b45ba2a222f6a10df034a87c5b8a3b1a66437
Facuindo <167240285+Facuindo@users.noreply.github.com>
2025-04-28 11:32:40 -0300

    Error

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int Distancia;
+    public int Peso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Peso > 100)
+        {
+            Debug.LogError("Error: El peso transportado no puede ser menor a cien kilos");
+        }
+        if (Distancia < 1)
+        {
+            Debug.LogError("Error: La distancia de transporte no puede ser menor a un kilometro");
+        }
+
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
